04dc0bce782ffdacb237c6dd74dedef814de1c44
Wise-deldu <delduwise@gmail.com>
2023-02-17 03:28:49 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This programs employs the puts function
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
