8353f16fbf16ff8c37775ef059b7e9c334c454ae
Wise-deldu <delduwise@gmail.com>
2023-04-11 01:49:01 +0000

    A function that prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - A program that prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	int v, comp = 0;
+	unsigned long int new;
+
+	for (v = 63; v >= 0; v--)
+	{
+		new = n >> v;
+
+		if (new & 1)
+		{
+			_putchar('1');
+			comp++;
+		}
+		else if (comp)
+			_putchar('0');
+	}
+	if (!comp)
+		_putchar('0');
+}
