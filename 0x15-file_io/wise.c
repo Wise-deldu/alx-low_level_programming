4171aee2fa28ec47139c9fa53ed111423f25394a
Wise-deldu <delduwise@gmail.com>
2023-04-15 23:22:44 +0000

    Just to commit

---
+++ 0x15-file_io/wise.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - Print a document for me.
+  *
+  * Return: always 0.
+  */
+
+int main(void)
+{
+	int a = 5;
+	int b = 8;
+
+	printf("The total of integers: %d\n", a + b);
+
+	return (0);
+}
